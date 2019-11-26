61e484bb0460c8bbcdc08519fa1f16f3f70876ba
Alendouro <miguelalendouro@gmail.com>
2019-11-26 11:55:53 +0000

    Initial Commit

---
+++ ReadBinary/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadBinary
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ReadingBinary("C:\\Users\\joao_\\data.bin");
+        }
+
+        private static void ReadingBinary(string path) {
+            //FileStream fs = new FileStream(path, FileMode.Open);
+            using (BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open))) {
+                while (br.BaseStream.Position != br.BaseStream.Length)
+                {
+                    int id = (byte)br.ReadInt32();
+                    float temperature = br.ReadSingle();
+                    float humidity = br.ReadSingle();
+                    int battery = (byte)br.ReadInt32();
+                    int timestamp = br.ReadInt32();
+                    DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(timestamp).ToLocalTime();
+                    string formattedDate = dt.ToString("dd-MM-yyyy HH:mm");
+                    int trash = br.ReadInt32();
+
+                    Console.WriteLine("Sensor ID = " + id);
+                    Console.WriteLine("Tenperature = " + temperature);
+                    Console.WriteLine("Humidity = " + humidity);
+                    Console.WriteLine("Batery = " + battery);
+                    Console.WriteLine("Data = " + formattedDate + "\n");
+                }  
+            }
+        }
+    }
+}
