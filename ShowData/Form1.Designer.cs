f7693e86172d8be10c694f6a63cd5ec6461a5a5f
Miguel Alendouro <miguelalendouro@gmail.com>
2019-12-03 17:28:13 +0000

    Show Data App
    -> Added the mqtt subscriber that receibes the values and present them

--- ShowData/Form1.Designer.cs
+++ ShowData/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTitleDesktopApp = new System.Windows.Forms.Label();
+            this.listBoxSensorsData = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelTitleDesktopApp
+            // 
+            this.labelTitleDesktopApp.AutoSize = true;
+            this.labelTitleDesktopApp.Font = new System.Drawing.Font("Bahnschrift SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitleDesktopApp.Location = new System.Drawing.Point(250, 9);
+            this.labelTitleDesktopApp.Name = "labelTitleDesktopApp";
+            this.labelTitleDesktopApp.Size = new System.Drawing.Size(274, 23);
+            this.labelTitleDesktopApp.TabIndex = 0;
+            this.labelTitleDesktopApp.Text = "Data From Sensors [Real Time]";
+            this.labelTitleDesktopApp.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // listBoxSensorsData
+            // 
+            this.listBoxSensorsData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxSensorsData.FormattingEnabled = true;
+            this.listBoxSensorsData.Location = new System.Drawing.Point(36, 73);
+            this.listBoxSensorsData.Name = "listBoxSensorsData";
+            this.listBoxSensorsData.Size = new System.Drawing.Size(711, 342);
+            this.listBoxSensorsData.TabIndex = 1;
+            this.listBoxSensorsData.SelectedIndexChanged += new System.EventHandler(this.ListBoxSensorsData_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxSensorsData);
+            this.Controls.Add(this.labelTitleDesktopApp);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitleDesktopApp;
+        private System.Windows.Forms.ListBox listBoxSensorsData;
     }
 }
 
--- ShowData/Form1.cs
+++ ShowData/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages; 
 
 namespace ShowData
 {
     public partial class Form1 : Form
     {
+        MqttClient mClient = new MqttClient("127.0.0.1"); //OR use the broker hostname
+        string[] mStrTopicsInfo = {"sensors"};
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mClient.Connect(Guid.NewGuid().ToString());
+            if (!mClient.IsConnected)
+            {
+                MessageBox.Show("Error connecting to message broker...");
+                return;
+            }
+            //Specify events we are interest on
+            //New Msg Arrived
+            mClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }; //QoS – depends on the topics number
+            //Subscribe to topics
+            mClient.Subscribe(mStrTopicsInfo, qosLevels);
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // We have to invoke because the mqttp works in a callback (so it doesn't have access to the elements)
+            this.Invoke( (MethodInvoker) delegate
+            {
+                listBoxSensorsData.Items.Add(Encoding.UTF8.GetString(e.Message));
+
+            });
+        }
+
+        private void ListBoxSensorsData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
